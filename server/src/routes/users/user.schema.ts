158ede676429c1838c139642c240b43943acbcf3
langchien <langtienk4@gmail.com>
2025-11-17 17:25:13 +0700

    Cập nhật message error cho user schema

--- server/src/routes/users/user.schema.ts
+++ server/src/routes/users/user.schema.ts
@@ -6,15 +6,27 @@ enum UserStatus {
   INACTIVE = 'inactive',
 }
 
-export const UserStatusSchema = z.enum(UserStatus)
+export const UserStatusSchema = z.enum(UserStatus, {
+  error: 'Trạng thái người dùng phải là active hoặc inactive',
+})
 
 export const UserSchema = z.object({
   id: createObjectIdSchema(),
-  username: z.string().min(3).max(30),
+  username: z
+    .string('Tên đăng nhập phải là 1 chuỗi ký tự tự')
+    .min(3, 'Tên đăng nhập phải có ít nhất 3 ký tự')
+    .max(30, 'Tên đăng nhập không được vượt quá 30 ký tự'),
   hashedPassword: z.string(),
-  displayName: z.string().min(3).max(50),
-  email: z.email(),
-  phone: z.string().min(10).max(15).optional(),
+  displayName: z
+    .string('Tên hiển thị phải là 1 chuỗi ký tự')
+    .min(3, 'Tên hiển thị phải có ít nhất 3 ký tự')
+    .max(50, 'Tên hiển thị không được vượt quá 50 ký tự'),
+  email: z.email('Email không hợp lệ'),
+  phone: z
+    .string('Số điện thoại phải là chuỗi ký tự')
+    .min(10, 'Số điện thoại phải có ít nhất 10 ký tự')
+    .max(15, 'Số điện thoại không được vượt quá 15 ký tự')
+    .optional(),
   status: UserStatusSchema,
   createdAt: z.coerce.date().default(() => new Date()),
   updatedAt: z.coerce.date().default(() => new Date()),
